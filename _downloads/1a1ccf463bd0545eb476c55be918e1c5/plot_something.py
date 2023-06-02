1880f5a95c7db255ae11a5a2d89542da9f327f24
ogrisel <ogrisel@users.noreply.github.com>
2023-06-02 17:33:20 +0000

    deploy: 565ef9cf164671ea9d005dde09f645f1f7f9c0f3

--- _downloads/1a1ccf463bd0545eb476c55be918e1c5/plot_something.py
+++ _downloads/1a1ccf463bd0545eb476c55be918e1c5/plot_something.py
@@ -5,11 +5,11 @@ A empty example
 TODO: this is just a placeholder for now
 """
 # %%
-import hazardous
+import hazardous  # noqa: F401
 import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-3, 3, 100)
-plt.plot(x, x ** 2, label="$x^2$")
-plt.plot(x, x ** 3, label="$x^3$")
+plt.plot(x, x**2, label="$x^2$")
+plt.plot(x, x**3, label="$x^3$")
 _ = plt.legend()
