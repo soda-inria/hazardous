354bd3aec626c19414b30e147fe6f313297498a1
Vincent M <maladiere.vincent@yahoo.fr>
2023-09-21 17:31:32 +0200

    init preprocessors

---
+++ hazardous/data/_seer_preprocess.py
@@ -0,0 +1,160 @@
+"""Helper to reproduce the SEER dataset transformations as performed in SurvTRACE.
+
+See: https://github.com/RyanWangZf/SurvTRACE/blob/main/data/process_seer.py
+"""
+import pandas as pd
+
+CAT_COLS = [
+    "Sex",
+    "Year of diagnosis",
+    "Race recode (W, B, AI, API)",
+    "Histologic Type ICD-O-3",
+    "Laterality",
+    "Sequence number",
+    "ER Status Recode Breast Cancer (1990+)",
+    "PR Status Recode Breast Cancer (1990+)",
+    "Summary stage 2000 (1998-2017)",
+    "RX Summ--Surg Prim Site (1998+)",
+    "Reason no cancer-directed surgery",
+    "First malignant primary indicator",
+    "Diagnostic Confirmation",
+    "Median household income inflation adj to 2019",
+]
+
+NUM_COLS = [
+    "Regional nodes examined (1988+)",
+    "CS tumor size (2004-2015)",
+    "Total number of benign/borderline tumors for patient",
+    "Total number of in situ/malignant tumors for patient",
+]
+
+COLS_MAPPING = {
+    0: "Patient ID",
+    1: "Sex",
+    2: "Year of diagnosis",
+    3: "Site recode ICD-0-3/WHO 2008",
+    4: "COD to site recode",
+    5: "Race recode (W, B, AI, API)",
+    6: "Histologic Type ICD-O-3",
+    7: "ICD-O-3 Hist/behav, malignant",
+    8: "Laterality",
+    9: "Sequence number",
+    10: "Vital status recode (study cutoff used)",
+    11: "ER Status Recode Breast Cancer (1990+)",
+    12: "PR Status Recode Breast Cancer (1990+)",
+    13: "Regional nodes examined (1988+)",
+    14: "xxx",
+    15: "Summary stage 2000 (1998-2017)",
+    16: "xxx",
+    17: "Reason no cancer-directed surgery",
+    18: "CS tumor size (2004-2015)",
+    19: "First malignant primary indicator",
+    20: "Diagnostic Confirmation",
+    21: "Total number of benign/borderline tumors for patient",
+    22: "Total number of in situ/malignant tumors for patient",
+    23: "Median household income inflation adj to 2019",
+    24: "RX Summ--Surg Prim Site (1998+)",
+    25: "SEER other cause of death classification",
+    26: "SEER cause-specific death classification",
+    27: "Survival months",
+    28: "xxx",
+}
+
+
+def preprocess_X_y(input_path, rename=True):
+    if rename:
+        df = pd.read_csv(input_path, sep="\t", header=None)
+        df = add_column_names(df)
+    else:
+        df = pd.read_csv(input_path)
+
+    mask = (
+        (df["SEER cause-specific death classification"] != "N/A not seq 0-59")
+        & (
+            df["Reason no cancer-directed surgery"]
+            != "Not performed, patient died prior to recommended surgery"
+        )
+        & (df["Survival months"] != "Unknown")
+    )
+    df = df.loc[mask]
+
+    df_features = preprocess_features(df)
+    df_events = preprocess_events(df)
+
+    return df_features, df_events
+
+
+def add_column_names(df):
+    """Name the columns of the SEER dataset, when missing from the extraction."""
+    df.rename(COLS_MAPPING, axis=1, inplace=True)
+    return df
+
+
+def preprocess_features(df):
+    df = df.drop(columns="Patient ID")
+
+    # Equivalent of OrdinalEncoder on frequencies of "Histologic Type ICD-O-3"
+    val_counts = df["Histologic Type ICD-O-3"].value_counts()
+    replace_dict = {}
+    for idx, count in enumerate(val_counts.values):
+        for k in val_counts[val_counts.values == count].index:
+            replace_dict[k] = str(idx)
+
+    df["Histologic Type ICD-O-3"].replace(replace_dict, inplace=True)
+
+    # Replace special missing values
+    df["ER Status Recode Breast Cancer (1990+)"].replace(
+        "Recode not available",
+        "Positive",
+        inplace=True,
+    )
+    df["PR Status Recode Breast Cancer (1990+)"].replace(
+        "Recode not available",
+        "Positive",
+        inplace=True,
+    )
+    df["Summary stage 2000 (1998-2017)"].replace(
+        "Unknown/unstaged",
+        "Localized",
+        inplace=True,
+    )
+    df["Reason no cancer-directed surgery"].replace(
+        "Unknown; death certificate; or autopsy only (2003+)",
+        "Surgery performed",
+        inplace=True,
+    )
+    df["Median household income inflation adj to 2019"].replace(
+        "Unknown/missing/no match/Not 1990-2018",
+        "$75,000+",
+        inplace=True,
+    )
+
+    df.replace("Unknown", None, inplace=True)
+
+    # TODO: specialize this logic between training and inference.
+    # During inference, we should fetch the trained object.
+    min_threshold = {
+        "Sequence number": 100,
+        "Diagnostic Confirmation": 160,
+    }
+    for col, min_freq in min_threshold.items():
+        val_count = df[col].value_counts()
+        low_freq_keys = val_count[val_count < min_freq].index
+        replace_dict = {k: low_freq_keys[0] for k in low_freq_keys[1:]}
+        df[col].replace(replace_dict, inplace=True)
+
+    return df
+
+
+def preprocess_events(df):
+    df = df.loc[df["Survival months"] != "Unknown"]
+    df = df.rename(columns={"Survival months": "duration"})
+
+    # if heart or alive, event = 0
+    target_encoded = {
+        "Breast": 1,
+        "Diseases of Heart": 2,
+    }
+    df["event"] = df["COD to site recode"].map(target_encoded).fillna(0)
+
+    return df[["event", "duration"]]
---
+++ hazardous/survtrace/_preprocessor.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+
+
+class FeaturePreprocessor(TransformerMixin, BaseEstimator):
+    def __init__(self, categ_cols, num_cols):
+        self.categ_cols = categ_cols
+        self.num_cols = num_cols
+
+    def fit(self, X, y=None):
+        self.col_transformer_ = make_column_transformer(
+            (OrdinalEncoder(), self.categ_cols),
+            (StandardScaler(), self.num_cols),
+            remainder="drop",
+            verbose_feature_names_out=False,
+        )
+        self.col_transformer_.set_output(transform="pandas")
+        self.col_transformer_.fit(X)
+
+        # Encode categorical values from different columns separately
+        # e.g. "blue" in column 1 is represented by a different token
+        # than "blue" in column 2.
+        transformers = self.col_transformer_.transformers_
+        categories = transformers[0][1].categories_
+        vocab_size = [0, *[len(categs) for categs in categories[:-1]]]
+        vocab_size = np.cumsum(vocab_size)
+
+        self.vocab_size_ = vocab_size
+
+        return self
+
+    def transform(self, X, y=None):
+        X_t = self.col_transformer_.transform(X)
+        X_t[self.categ_cols] += self.vocab_size_
+
+        return X_t
+
+
+class TargetPreprocessor(TransformerMixin, BaseEstimator):
+    def __init__(self, horizons):
+        self.horizons = horizons
+
+    def fit(self, y):
+        # Check strictly increasing
+        if not np.all(self.horizons[:-1] < self.horizons[1:]):
+            raise ValueError(
+                f"'horizon' must be strictly increasing, got {self.horizons=!r}"
+            )
+
+        # Defining cuts
+        mask_first_event = y["event"] == 1
+        first_event_durations = y.loc[mask_first_event]["duration"]
+        times = np.quantile(first_event_durations, self.horizons).tolist()
+        max_duration = y["duration"].max()
+        self.cuts_ = np.array([0, *times, max_duration])
+        self.cuts_to_idx_ = dict(zip(self.cuts_, range(len(self.cuts_))))
+
+        return self
+
+    def transform(self, y):
+        durations = y["duration"]
+        events = y["event"]
+
+        # Apply right censoring when
+        max_cut = self.cuts_.max()
+        censor = durations > max_cut
+        durations[censor] = max_cut
+        events[censor] = 0
+
+        # Binning the durations
+        bins = np.searchsorted(self.cuts_, durations, side="left")
+        binned_durations = self.cuts_[bins]
+        idx_durations = np.array([self.cuts_to_idx_[bin] for bin in binned_durations])
+
+        cut_diff = np.diff(self.cuts_)
+        frac_durations = (
+            1.0 - (binned_durations - durations) / cut_diff[idx_durations - 1]
+        )
+
+        # Event or censoring at start time should be removed.
+        # We left them at zero so that they don't contribute to the loss.
+        mask_zero_duration = idx_durations == 0
+        frac_durations[mask_zero_duration] = 0
+        events[mask_zero_duration] = 0
+
+        return pd.DataFrame(
+            dict(
+                event=events.astype("int64"),
+                duration=idx_durations.astype("int64") - 1,
+                frac_duration=frac_durations.astype("float32"),
+            )
+        )
