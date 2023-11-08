cc08eaca718a0fde5d29125dd92a243c349f1cf5
Julie <107467312+juAlberge@users.noreply.github.com>
2023-11-08 15:59:53 +0100

    Apply suggestions from code review
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- hazardous/_gradient_boosting_incidence.py
+++ hazardous/_gradient_boosting_incidence.py
@@ -90,16 +90,21 @@ class GradientBoostingIncidence(BaseEstimator, ClassifierMixin):
     .. math::
 
         \hat{F}_k(t) \approx \mathbb{P}(T \leq t, E=k)
-
+    where :math:`T` is a random variable for the uncensored time to first event
+    and :math:`E` is a random variable over the :math:`[1, K]` domain for the
+    uncensored event type.
     One can obtain the survival probability of any event by computing
-    each CIF (that is to say running the model
-    for each cause-specific CIF) and computing 1 - “sum of CIF curves” because:
+    each CIF (that is to say running the model for each cause-specific CIF)
+    and computing 1 - “sum of CIF curves” because:
 
     .. math::
 
         S(t) = \mathbb{P}(T > t) = 1 - \mathbb{P}(T \leq t)
         = 1 - \sum_{k=1}^K \mathbb{P}(T \leq t, E=k)
         \approx 1 - \sum_{k=1}^K \hat{F}_k(t)
+
+    Note that this is equivalent in expectation to running the model once
+    for the collapsed `event_of_intercept="any"` formulation.
 
     This internally relies on the histogram-based gradient boosting classifier
     or regressor implementation of scikit-learn.
