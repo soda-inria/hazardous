7a461a0c259e012fe436f9d9bf6320615c8d964d
Théo Jolivet <theo.jolivet@ens-paris-saclay.fr>
2024-11-27 16:31:08 +0100

    Update test size

--- examples/plot_01_survival_analysis.py
+++ examples/plot_01_survival_analysis.py
@@ -48,7 +48,7 @@ y["event"].value_counts(normalize=True)
 # %%
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 # %%
 #
